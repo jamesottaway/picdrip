777408af904cf652fb5343de4932c3ba18b9a535
James Ottaway <james@ottaway.mp>
2011-03-01 21:06:26 +1000

    Symbolised config.yml

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,5 +1,5 @@
 CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
 
-APP_CONFIG = {:tmp_dir => CONFIG['app']['tmp_dir']}
-FLICKR_CONFIG = {:api_key => ENV['FLICKR_API_KEY'] || CONFIG['flickr']['api_key'], :api_secret => ENV['FLICKR_API_SECRET'] || CONFIG['flickr']['api_secret']}
-TWITTER_CONFIG = {:consumer_key => ENV['TWITTER_CONSUMER_KEY'] || CONFIG['twitter']['consumer_key'], :consumer_secret => ENV['TWITTER_CONSUMER_SECRET'] || CONFIG['twitter']['consumer_secret']}
+APP_CONFIG = {:tmp_dir => CONFIG[:app][:tmp_dir]}
+FLICKR_CONFIG = {:api_key => ENV['FLICKR_API_KEY'] || CONFIG[:flickr][:api_key], :api_secret => ENV['FLICKR_API_SECRET'] || CONFIG[:flickr][:api_secret]}
+TWITTER_CONFIG = {:consumer_key => ENV['TWITTER_CONSUMER_KEY'] || CONFIG[:twitter][:consumer_key], :consumer_secret => ENV['TWITTER_CONSUMER_SECRET'] || CONFIG[:twitter][:consumer_secret]}
