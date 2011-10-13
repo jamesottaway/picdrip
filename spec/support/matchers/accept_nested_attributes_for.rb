828030a30866aae566b55ec0fc0079b64d91b1d9
James Ottaway <james@ottaway.mp>
2011-10-14 08:47:27 +1000

    Upgraded to 1.9.2

--- spec/support/matchers/accept_nested_attributes_for.rb
+++ spec/support/matchers/accept_nested_attributes_for.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :accept_nested_attributes_for do |attribute|
   match do |model|
-    model.methods.include? "#{attribute}_attributes="
+    model.methods.include? "#{attribute}_attributes=".to_sym
   end
 
   failure_message_for_should do |model|
