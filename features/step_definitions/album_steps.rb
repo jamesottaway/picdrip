a8c240da85b3168f78eff006e62d3afdfaa758e5
James Ottaway <james@ottaway.mp>
2011-10-19 14:14:21 +1000

    Removed some duplicate code

--- features/step_definitions/album_steps.rb
+++ features/step_definitions/album_steps.rb
@@ -1,25 +1,11 @@
 Given /^I have created an Album$/ do
-  on :albums do |page|
-    page.new_album
-  end
-  on :new_album do |page|
-    page.title = 'Photo Management Cucumber'
-    page.description = 'This album is for the Cucumber features in photos.feature'
-    page.create
-    page.notice.should == 'Album was successfully created.'
-  end
+  create_album :title => 'Photo Management Cucumber',
+    :description => 'This album is for the Cucumber features in photos.feature'
 end
 
 When /^I create an Album$/ do
-  on :albums do |page|
-  	page.new_album
-  end
-  on :new_album do |page|
-  	page.title = 'New Album'
-  	page.description = 'Thanks Picdrip!'
-  	page.create
-  	page.notice.should == 'Album was successfully created.'
-  end
+  create_album :title => 'New Album',
+    :description => 'Thanks Picdrip!'
 end
 
 Then /^I should be able to view the Album$/ do
@@ -27,5 +13,16 @@ Then /^I should be able to view the Album$/ do
     page.browser.title.should =~ /^New Album/
     page.title.should == 'New Album'
     page.description.should == 'Thanks Picdrip!'
+  end
+end
+
+def create_album opts
+  on :albums do |page|
+    page.new_album
+  end
+  on :new_album do |page|
+    opts.each { |key, value| page.send "#{key}=", value }
+    page.create
+    page.notice.should == 'Album was successfully created.'
   end
 end
