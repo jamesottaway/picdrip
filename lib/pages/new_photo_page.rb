de0c516ed4c15d44049efd2b628b7eaca259f819
James Ottaway <james@ottaway.mp>
2011-10-21 08:51:34 +1000

    Upgraded watir-page-helper to get rid of my file_field hack

--- lib/pages/new_photo_page.rb
+++ lib/pages/new_photo_page.rb
@@ -1,9 +1,6 @@
 class NewPhotoPage < BasePageClass
   text_field :title, :name => 'photo[title]'
   text_field :description, :name => 'photo[description]'
+  file_field :photo, :name => 'photo[s3]'
   button :create, :type => 'submit'
-  
-  def photo= filename
-  	file_field(:name, 'photo[s3]').set filename
-  end
 end
