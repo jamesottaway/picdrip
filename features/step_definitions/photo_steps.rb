deef280de061bee0224b6e55a75d969370fc186a
James Ottaway <james@ottaway.mp>
2011-10-21 10:53:56 +1000

    Given I have uploaded a Photo

--- features/step_definitions/photo_steps.rb
+++ features/step_definitions/photo_steps.rb
@@ -1,14 +1,13 @@
+Given /^I have uploaded a Photo$/ do
+  create_photo :title => 'Photo To Be Uploaded',
+    :description => 'This should be uploaded by Picdrip',
+    :photo => root_path + 'public/images/rails.png'
+end
+
 When /^I upload a new photo$/ do
-  on :album do |page|
-  	page.new_photo
-  end
-  on :new_photo do |page|
-    page.title = 'New Photo'
-    page.description = "Wow, it's so easy!"
-    page.photo = root_path + 'public/images/rails.png'
-    page.create
-    page.notice.should == 'Photo was successfully created.'
-  end
+  create_photo :title => 'New Photo',
+    :description => "Wow, it's so easy!",
+    :photo => root_path + 'public/images/rails.png'
 end
 
 Then /^I should be able to view the Photo$/ do
@@ -16,5 +15,16 @@ Then /^I should be able to view the Photo$/ do
   	page.browser.title.should =~ /^New Photo/
   	page.title.should == 'New Photo'
   	page.description.should == "Wow, it's so easy!"
+  end
+end
+
+def create_photo opts
+  on :album do |page|
+    page.new_photo
+  end
+  on :new_photo do |page|
+    opts.each { |key, value| page.send "#{key}=", value }
+    page.create
+    page.notice.should == 'Photo was successfully created.'
   end
 end
